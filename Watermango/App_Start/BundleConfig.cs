3590a6181d58a7adff711d77d44f36fc5df30cea
Jayme Bandeira Cunha <jaymecunha@gmail.com>
2019-09-17 16:01:41 -0400

    Clean homepage and add vuejs

--- Watermango/App_Start/BundleConfig.cs
+++ Watermango/App_Start/BundleConfig.cs
@@ -17,9 +17,7 @@ namespace Watermango
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/popper.js"
-                      ));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
